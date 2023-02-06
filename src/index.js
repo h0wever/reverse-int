bd44990f0711a9b6359e34e3f96e8cf45b267a10
Siarhei <107548242+h0wever@users.noreply.github.com>
2023-02-06 20:48:22 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(a) {
+    let int = function positive(a) {
+        if (a < 0) {
+            return a * -1;
+        } else {
+            return a;
+        }
+    };
+
+    const reverse = Number(
+        String(int(a)).split("").map(Number).reverse().join("")
+    );
+};
